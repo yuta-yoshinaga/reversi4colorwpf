a2319668439b94d29502efe6fda42aed089ea104
吉永　祐太 <yuta.yoshinaga@gmail.com>
2018-09-15 23:22:02 +0900

    docsフォルダ追加。

--- Doxygen/html/search/classes_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['app',['App',['../class_reversi4color_wpf_1_1_app.html',1,'Reversi4colorWpf']]],
-  ['app',['App',['../class_reversi_wpf_1_1_app.html',1,'ReversiWpf']]]
-];
---
+++ docs/search/all_b.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['m_5fappsettings',['m_AppSettings',['../class_reversi4color_wpf_1_1_main_window.html#ac917348d0c9b1a6a4ec17482eb0b856f',1,'Reversi4colorWpf::MainWindow']]],
+  ['m_5freversiplay',['m_ReversiPlay',['../class_reversi4color_wpf_1_1_main_window.html#a8393046a6166856f68fe472fb58894c4',1,'Reversi4colorWpf::MainWindow']]],
+  ['main',['Main',['../class_reversi_wpf_1_1_app.html#a21f38c0f40a47a04302edeeb7bf005f8',1,'ReversiWpf.App.Main()'],['../class_reversi_wpf_1_1_app.html#a21f38c0f40a47a04302edeeb7bf005f8',1,'ReversiWpf.App.Main()'],['../class_reversi_wpf_1_1_app.html#a21f38c0f40a47a04302edeeb7bf005f8',1,'ReversiWpf.App.Main()'],['../class_reversi_wpf_1_1_app.html#a21f38c0f40a47a04302edeeb7bf005f8',1,'ReversiWpf.App.Main()']]],
+  ['mainwindow',['MainWindow',['../class_reversi4color_wpf_1_1_main_window.html',1,'Reversi4colorWpf.MainWindow'],['../class_reversi4color_wpf_1_1_main_window.html#ad472ea40f8c1574627f7d855cd6515f1',1,'Reversi4colorWpf.MainWindow.MainWindow()']]],
+  ['mainwindow_2examl_2ecs',['MainWindow.xaml.cs',['../_main_window_8xaml_8cs.html',1,'']]],
+  ['makemasusts',['makeMasuSts',['../class_reversi4color_wpf_1_1_my_reversi.html#aacb2634fe75f3290910033d13bd335aa',1,'Reversi4colorWpf::MyReversi']]],
+  ['myreversi',['MyReversi',['../class_reversi4color_wpf_1_1_my_reversi.html',1,'Reversi4colorWpf.MyReversi'],['../class_reversi4color_wpf_1_1_my_reversi.html#a0aff82171fb2635b92c341d975ef0821',1,'Reversi4colorWpf.MyReversi.MyReversi()']]],
+  ['myreversi_2ecs',['MyReversi.cs',['../_my_reversi_8cs.html',1,'']]]
+];
